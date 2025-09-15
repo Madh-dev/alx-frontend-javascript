d32c3d3e9aace8d2eb80aff4701b53f855055957
ALEEM ABDULMUHEEZ Ayomide <aleemabdulmuheez@gmail.com>
2025-09-15 14:45:24 +0100

    updated

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,10 +46,10 @@ return 'Getting to work';
 // }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary >= 500) {
-    return new Director();
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
   }
-  return new Teacher();
+  return new Director();
 }
 
 
